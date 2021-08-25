1af1b1cabfdf71000e76d001d5ab22f05161f693
zhaoyang1314 <1143011510@qq.com>
2021-08-25 11:06:36 +0800

    网关集成权鉴

---
+++ gataway-security/src/main/java/com/gataway/gatawaysecurity/GatawaySecurityApplication.java
@@ -0,0 +1,13 @@
+package com.gataway.gatawaysecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatawaySecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatawaySecurityApplication.class, args);
+    }
+
+}
---
+++ gataway-security/src/test/java/com/gataway/gatawaysecurity/GatawaySecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gataway.gatawaysecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatawaySecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
